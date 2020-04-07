25abfff428093b677eec398c2ffe988b076ded6f
sergionet17 <sergionet17@gmail.com>
2020-04-06 20:30:22 -0500

    nuevoDependencias

---
+++ dependencias/src/test/java/com/ejemplo2/injection/dependecies/dependencias/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo2.injection.dependecies.dependencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
